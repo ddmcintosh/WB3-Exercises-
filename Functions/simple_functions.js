c8c19ae1b4edfe8b618d2673de571fa7d0e3af12
Your Name <you@example.com>
2022-10-24 13:34:08 -0400

    Fave Things, Why I'm Here, & Fave Country

---
+++ Functions/simple_functions.js
@@ -0,0 +1,26 @@
+
+
+function displayFavoriteThings(name, favoritemovie, musician) {
+    let message = " My name is " + name + ", my favorite movie is " + favoritemovie + " , and my favorite artist is " + musician
+    console.log(message); 
+}
+
+let thename = "Deavion"
+let favoritemovie = "Love & Basketball"
+let musician = "Jhene Aiko"
+displayFavoriteThings(thename,favoritemovie, musician );
+
+
+function displayWhyImHere(myWhy) {
+    let message = " I'm at Year up because " +myWhy
+    console.log(message);
+}
+let myWhy = "I want to break into tech, become a software engineer and increase my pay"
+displayWhyImHere(myWhy);
+
+function displayFavoritePlace(bestCountry) {
+    let message = " My favorite country is " + bestCountry
+    console.log(message);
+}
+let bestCountry = "Dubai"
+displayFavoritePlace(bestCountry);
