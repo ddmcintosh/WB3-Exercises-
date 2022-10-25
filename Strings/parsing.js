f3c9031dc236b83aed80d18cf2b1334a0430a33e
Your Name <you@example.com>
2022-10-25 11:43:58 -0400

    completed example

---
+++ Strings/parsing.js
@@ -0,0 +1,22 @@
+"use strict";
+
+function parseAndDisplayName(name) {
+    let pos = name.indexOf(" ");
+    let firstName = name.substring(0, pos);
+    let lastName = name.substring(pos);
+    
+    console.log("Name: " + name);
+    console.log("First name: " + firstName);
+    console.log("Last name: " + lastName);
+    console.log("");  
+}
+
+
+
+
+parseAndDisplayName("Brenda Kaye");
+parseAndDisplayName("Ian Auston");
+parseAndDisplayName("Siddalee Grace");
+
+
+
