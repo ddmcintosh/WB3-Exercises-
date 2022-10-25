ab2f07da31bd659d064b2f3074823a3a17b63158
Your Name <you@example.com>
2022-10-25 10:37:26 -0400

    added data twice for two people on mailing

--- Functions/more_functions.js
+++ Functions/more_functions.js
@@ -11,6 +11,25 @@ let thezip = "10004"
 displayMailingLabel(thename, theaddress, thecity, thestate, thezip);
 
 
+function displayMailingLabel(name, address, city, state, zip) {
+    let message = " My name is " +  name + " I live at " + address + " the city is " + city + " the state is " + state + " and the zipcode is " + zip
+    console.log(message);
+} 
+
+let thename2 = "Marie"
+let theaddress2 = "85 Broad St"
+let thecity2 = "NYC"
+let thestate2 = "NY"
+let thezip2 = "10000"
+
+console.log(thename2);
+console.log(theaddress2);
+console.log(thecity2);
+console.log(thestate2);
+console.log(thezip2);
+
+displayMailingLabel(thename2, theaddress2, thecity2, thestate2, thezip2);
+
 
 
 
@@ -30,3 +49,8 @@ function displayReceipt(totalDue, amountPaid ) {
 let thetotalDue = "$27.00"
 let theamountPaid = "$30.00"
 displayReceipt(thetotalDue, theamountPaid);
+
+
+
+
+
