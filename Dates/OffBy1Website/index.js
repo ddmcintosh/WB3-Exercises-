a58d443b4fee636ca7d1bcf24ce72609a4708795
Your Name <you@example.com>
2022-10-26 12:15:02 -0400

    Completed OffBy1Website Exercise

---
+++ Dates/OffBy1Website/index.js
@@ -0,0 +1,34 @@
+// document has strict laws
+"use strict"
+
+//when the window loads this will be initated
+window.onload = init;
+
+
+//defining init and linking button 
+
+function init()
+{
+    const btn = document.getElementById("btn");
+    btn.onclick = btnOnClicked;
+}
+
+
+
+// what happens when button is clicked
+
+function btnOnClicked() {
+console.log("The button was clicked");
+
+//dateString is what person is putting in 
+ let dateString = document.getElementById(dateField.value);
+ let date = new Date(dateString);
+ console.log(new Date(dateString));
+
+
+}
+
+
+
+
+
