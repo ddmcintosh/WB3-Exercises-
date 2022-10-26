043ed50f68787ab2ea9a860ee62d654f8e3e5cc3
Your Name <you@example.com>
2022-10-26 09:11:57 -0400

    Replacing String Exercise

---
+++ Strings/replace_strings.js
@@ -0,0 +1,12 @@
+"use strict";
+
+
+function replace(message)
+{
+    let newmessage = message.replace("Dallas", "Austin");
+
+    console.log(message)
+    console.log(newmessage)
+}
+
+replace("Our corporate offices are located in Dallas");
